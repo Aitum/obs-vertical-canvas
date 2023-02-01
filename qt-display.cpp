0796790cc7f3500490edecbbc79e233d022ddcfe
Exeldro <exelsor@hotmail.com>
2023-02-01 21:17:00 +0100

    fix linux compile error

--- qt-display.cpp
+++ qt-display.cpp
@@ -167,21 +167,19 @@ bool QTToGSWindow(QWindow *window, gs_window &gswindow)
 #elif __APPLE__
 	gswindow.view = (id)window->winId();
 #else
-	switch (obs_get_nix_platform()) {
-	case OBS_NIX_PLATFORM_X11_EGL:
+	if (obs_get_nix_platform() == OBS_NIX_PLATFORM_X11_EGL) {
 		gswindow.id = window->winId();
 		gswindow.display = obs_get_nix_platform_display();
-		break;
+	}
 #ifdef ENABLE_WAYLAND
-	case OBS_NIX_PLATFORM_WAYLAND:
+	if (obs_get_nix_platform() == OBS_NIX_PLATFORM_WAYLAND) {
 		QPlatformNativeInterface *native =
 			QGuiApplication::platformNativeInterface();
 		gswindow.display =
 			native->nativeResourceForWindow("surface", window);
 		success = gswindow.display != nullptr;
-		break;
-#endif
 	}
+#endif
 #endif
 	return success;
 }
