d5e4ef36456581a37e1fb28c9e366836c9b960d7
Exeldro <exelsor@hotmail.com>
2025-09-20 07:38:08 +0200

    Fix crashes on exit

--- source-tree.cpp
+++ source-tree.cpp
@@ -801,6 +801,8 @@ void SourceTreeModel::Add(obs_sceneitem_t *item)
 
 void SourceTreeModel::Remove(obs_sceneitem_t *item)
 {
+	if (std::find(canvas_docks.begin(), canvas_docks.end(), st->canvasDock) == canvas_docks.end())
+		return;
 	int idx = -1;
 	for (int i = 0; i < items.count(); i++) {
 		if (items[i] == item) {
@@ -1560,6 +1562,8 @@ bool SourceTree::GroupedItemsSelected() const
 
 void SourceTree::Remove(OBSSceneItem item, OBSScene scene)
 {
+	if (std::find(canvas_docks.begin(), canvas_docks.end(), canvasDock) == canvas_docks.end())
+		return;
 	GetStm()->Remove(item);
 	obs_frontend_save();
 
